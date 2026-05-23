1d801cc20d607e9ff6d5f06ed930e67e721788f6
William Whatley <wwwhatley@icloud.com>
2026-05-22 22:48:36 -0500

    Rename praxis -> Praxis and praxisClient -> PraxisClient

--- src/main/java/io/github/renegadeactual/praxis/PraxisClient.java
+++ src/main/java/io/github/renegadeactual/praxis/PraxisClient.java
@@ -11,11 +11,11 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = praxis.MODID, dist = Dist.CLIENT)
+@Mod(value = Praxis.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = praxis.MODID, value = Dist.CLIENT)
-public class praxisClient {
-    public praxisClient(ModContainer container) {
+@EventBusSubscriber(modid = Praxis.MODID, value = Dist.CLIENT)
+public class PraxisClient {
+    public PraxisClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
@@ -25,7 +25,7 @@ public class praxisClient {
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        praxis.LOGGER.info("HELLO FROM CLIENT SETUP");
-        praxis.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        Praxis.LOGGER.info("HELLO FROM CLIENT SETUP");
+        Praxis.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
